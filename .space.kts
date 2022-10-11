84e1451fe76710c1bb3599dc8c4a97cfc63c57f6
Yuhinng ng <yuhinng41@gmail.com>
2022-10-11 14:39:02 +0000

    add sequential

--- .space.kts
+++ .space.kts
@@ -4,33 +4,34 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 job("test_php_here") {
-    container(displayName="Say Hello" , image = "composer") {
-        resources {
-        	cpu = 1.cpu
-        	memory = 2000.mb
+    sequential {
+        container(displayName="Say Hello", image = "composer") {
+            resources {
+                cpu = 1.cpu
+                memory = 2000.mb
+            }
+            shellScript {
+                content = """
+                    echo hello
+                    echo world!!
+                    ls -a
+                    php --version
+
+                    composer --version
+                    composer install
+                    ls -a
+                    vendor/bin/phpunit .
+                """
+            }
         }
-    	shellScript {
-        	content = """
-        		echo hello
-                echo world!!
-                ls -a
-                php --version
-                
-                composer --version
-                composer install
-                ls -a
-                vendor/bin/phpunit .
-            """
-        }
-    }
-}
-job("testing ls") {
-    container (displayName="testing ls", image = "ubuntu") {
-    	shellScript {
-        	content = """
-            	echo Hello World!
-                ls -la
-            """
+
+        container (displayName="testing ls", image = "ubuntu") {
+            shellScript {
+                content = """
+                    echo Hello World!
+                    ls -la
+                """
+            }
         }
     }
 }
