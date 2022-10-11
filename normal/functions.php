65a4a2de60cbea60756d69a1c491d2a768176cfc
BcnChsBrgr <91963960+BcnChsBrgr@users.noreply.github.com>
2022-10-11 12:35:03 +0100

    Create functions.php

---
+++ normal/functions.php
@@ -0,0 +1,11 @@
+<?php 
+
+namespace Normal\functions;
+// return empty if input is not array
+function bobbleSort(array $tmp):array
+{
+  if(is_array($tmp)) {
+  // do something
+  }
+  return [];
+}
