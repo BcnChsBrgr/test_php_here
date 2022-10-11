a0765542a8676fe27903f2d0b3a1e34abbcd77ca
Yuhinng ng <yuhinng41@gmail.com>
2022-10-11 15:01:13 +0000

    ' to "

--- .space.kts
+++ .space.kts
@@ -11,8 +11,8 @@ job("test_php_here") {
             memory = 2000.mb
         }
         shellScript {
-            env['TEST_PARAM'] = Params('test_param')
-            env['THIS_IS_SECRET'] = Secrets('this_is_secret')
+            env["TEST_PARAM"] = Params("test_param")
+            env["THIS_IS_SECRET"] = Secrets("this_is_secret")
             content = """
             		echo ${'$'}TEST_PARAM
                     echo ${'$'}THIS_IS_SECRET
