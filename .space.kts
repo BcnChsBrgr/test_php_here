bb6d35b8172a442e8a1dd9fa0afde3526eb32cb8
Yuhinng ng <yuhinng41@gmail.com>
2022-10-11 14:52:40 +0000

    adding JB_SPACE_WORK_DIR_PATH

--- .space.kts
+++ .space.kts
@@ -10,12 +10,9 @@ job("test_php_here") {
             cpu = 1.cpu
             memory = 2000.mb
         }
-        kotlinScript { api->
-        	println("Build #"+api.executionNumber())
-        }
         shellScript {
             content = """
-                    echo ${'$'}JB_SPACE_EXECUTION_NUMBER
+                    echo ${'$'}JB_SPACE_WORK_DIR_PATH
                     ls -a
                     php --version
 
