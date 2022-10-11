5ea24b672af52318a488c25213555c02b69f08a3
Yuhinng ng <yuhinng41@gmail.com>
2022-10-11 13:45:24 +0000

    change image in docker

--- .space.kts
+++ .space.kts
@@ -4,11 +4,12 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 job("test_php_here") {
-    container(displayName="Say Hello" , image = "ubuntu") {
+    container(displayName="Say Hello" , image = "php:8.1.5-fpm") {
     	shellScript {
         	content = """
         		echo hello
                 echo world!!
+                ls -a
             """
         }
     }
