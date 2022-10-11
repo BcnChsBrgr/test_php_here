ac5189343f7e32ac648c3e5f8ccfea44a5e66834
Yuhinng ng <yuhinng41@gmail.com>
2022-10-11 13:32:52 +0000

    hello world

--- .space.kts
+++ .space.kts
@@ -4,18 +4,5 @@
 * For more info, see https://www.jetbrains.com/help/space/automation.html
 */
 job("test_php_here") {
-    // check-out main
-    git("test_php_here")
-    container(displayName = "Show dir", image = "amazoncorretto:17-alpine") {
-        shellScript {
-            content = """
-                echo Directory structure
-                ls -R /mnt
-                echo Working dir is
-                pwd
-            """
-        }
-    }
-    // If the main repository is 'main-repo', then
-    // working dir is /mnt/space/work/main-repo
+    container(displayName="Say Hello" , image = "hello - world")
 }
